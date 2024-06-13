1f58d0ede39006fa8ba4adc61aef6f2220e12cfa
GURUNATH-DESHMUKH <gurunath.deshmukh@bajajfinserv.in>
2024-06-13 18:04:45 +0530

    Few algorithms for singly linked list added

---
+++ src/main/dsa/algorithms/linked_list/singly_linked_list/traverse/Main.java
@@ -0,0 +1,22 @@
+package src.main.dsa.algorithms.linked_list.singly_linked_list.traverse;
+
+import src.main.dsa.algorithms.linked_list.singly_linked_list.Node;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Node end = new Node("End", null);
+        Node legs = new Node("Legs", end);
+        Node torse = new Node("Torso", legs);
+        Node head = new Node("Head", torse);
+
+        Node current = head;
+        System.out.println("Beginning to traverse the list");
+        while (current != null) {
+            System.out.println("Currently on : " + current.getData());
+            current = current.getNext();
+        }
+        System.out.println("Traversal completed for the list");
+    }
+
+}
